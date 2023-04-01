45ef6f9182a57ab6f291f30a2e9be09465571540
Brian Jimerson <64101689+btjimerson@users.noreply.github.com>
2023-04-01 09:35:59 -0400

    Add kubeconfig to cloud provider

--- src/main/java/xyz/pintobean/yba/domain/AdminUserKubernetesEntity.java
+++ src/main/java/xyz/pintobean/yba/domain/AdminUserKubernetesEntity.java
@@ -6,29 +6,37 @@ public class AdminUserKubernetesEntity {
     private String password;
     private String email;
     private String environment;
-    
+
     public String getFullName() {
         return fullName;
     }
+    
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+    
     public String getPassword() {
         return password;
     }
+    
     public void setPassword(String password) {
         this.password = password;
     }
+    
     public String getEmail() {
         return email;
     }
+    
     public void setEmail(String email) {
         this.email = email;
     }
+    
     public String getEnvironment() {
         return environment;
     }
+    
     public void setEnvironment(String environment) {
         this.environment = environment;
     }
+    
 }
