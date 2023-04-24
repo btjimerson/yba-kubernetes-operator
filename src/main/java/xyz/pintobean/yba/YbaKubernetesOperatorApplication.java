097c55a61ec5f84d4658386b6b8ed5ed0d296606
Brian Jimerson <64101689+btjimerson@users.noreply.github.com>
2023-04-24 13:24:20 -0400

    Fix main application name

--- src/main/java/xyz/pintobean/yba/YbaKubernetesOperatorApplication.java
+++ src/main/java/xyz/pintobean/yba/YbaKubernetesOperatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YbaKubernetesManagerApplication {
+public class YbaKubernetesOperatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YbaKubernetesManagerApplication.class, args);
+		SpringApplication.run(YbaKubernetesOperatorApplication.class, args);
 	}
 	
 }
